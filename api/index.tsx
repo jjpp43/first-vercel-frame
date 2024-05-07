060a0ba7c047c200b61d261d05aac5d36968e4ae
jjpp43 <jparkdev63@gmail.com>
2024-05-08 08:58:38 +0900

    First commit

---
+++ api/index.tsx
@@ -0,0 +1,98 @@
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+import { serveStatic } from 'frog/serve-static'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/vercel'
+
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+
+export const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+})
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background:
+            status === 'response'
+              ? 'linear-gradient(to right, #432889, #17101F)'
+              : 'black',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          {status === 'response'
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+            : 'Welcome!'}
+        </div>
+      </div>
+    ),
+    intents: [
+      <Button value="left">치킨무없이 치킨</Button>,
+      <Button value="right">케찹없이 감튀</Button>,
+    ],
+  })
+})
+
+
+app.frame('/picker', (c) => {
+  const leftFrame = 'http://localhost:5173/left/frame_left.png'
+  const rightFrame = 'http://localhost:5173/right/frame_right.png'
+  const { buttonValue } = c
+  if (buttonValue === 'left') {
+    return c.res({
+      action: '/',
+      image: leftFrame,
+      intents: [
+        <Button value="reset">다시 하기</Button>,
+      ],
+    })
+  } else {
+    return c.res({
+      action: '/',
+      image: rightFrame,
+      intents: [
+        <Button value="reset">다시 하기</Button>,
+      ]
+    })
+  }
+
+})
+
+
+// @ts-ignore
+const isEdgeFunction = typeof EdgeFunction !== 'undefined'
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
+devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
